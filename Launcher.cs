5b49b25d80fdf311d36254c9819d710519c6aeb3
vladimir <sachkovvi@shm.ru>
2025-01-30 22:28:44 +0300

    Пройден урок: ДВУМЕРНЫЙ МАССИВ | ОБЪЯВЛЕНИЕ | ИНИЦИАЛИЗАЦИЯ | ИНДЕКСЫ | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | УРОК # 30

--- Launcher.cs
+++ Launcher.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SimpleCode_Course_Part_2;
 
-Lesson_29 lesson_29 = new Lesson_29();
+Lesson_30 lesson_30 = new Lesson_30();
 
-lesson_29.Start();
+lesson_30.Start();
---
+++ Lessons/Lesson_30.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCode_Course_Part_2
+{
+    class Lesson_30
+    {
+        /*
+         * инициализация
+         * 
+         * обращение к элементам двумерного массива
+         */
+
+        public Lesson_30()
+        {
+        }
+
+        public void Start()
+        {
+            Start_1();
+        }
+
+        private void Start_1()
+        {
+            // Тип данных [,] имя массива
+            //int[,] myArray;
+            //myArray = new int[3, 5];
+
+            //int[,] myArray = new int[3, 5];
+
+            //myArray[2, 2] = 99;
+
+            //Console.WriteLine(myArray[10, 2]);
+
+            //int[,] myArray = new int[3, 5] {
+            //    {2, 45, 12, 51, 51}, 
+            //    {6, 7, 8, 3, 1}, 
+            //    {1, 5, 65, 78, 13},
+            //};
+
+
+            int[,] myArray = new int[,] {
+                {2, 45, 12, 51, 51},
+                {6, 7, 8, 3, 1},
+                {1, 5, 65, 78, 13},
+                {6, 3 , 151, 5, 65 }
+            };
+        }
+
+        //private void Start_2() 
+        //{
+            
+        //}
+    }
+}
