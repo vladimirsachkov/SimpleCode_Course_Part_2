39799016a60fbbfd30b833c1089b41bcfb43d33b
vladimir <sachkovvi@shm.ru>
2025-01-30 09:55:04 +0300

    Пройден урок: ДВУМЕРНЫЙ МАССИВ В C# | МНОГОМЕРНЫЕ МАССИВЫ В СИ ШАРП | ЧТО ТАКОЕ МАССИВЫ | ИЗУЧЕНИЕ C# | УРОК # 29

--- Launcher.cs
+++ Launcher.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SimpleCode_Course_Part_2;
 
-Lesson_28 lesson_28 = new Lesson_28();
+Lesson_29 lesson_29 = new Lesson_29();
 
-lesson_28.Start();
+lesson_29.Start();
---
+++ Lessons/Lesson_29.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCode_Course_Part_2
+{
+    /*
+     * многомерные массивы
+     * 
+     * двумерный прямоугольный массив
+     */
+
+    class Lesson_29
+    {
+        public Lesson_29()
+        {
+        }
+
+        public void Start()
+        {
+            Start_1();
+        }
+
+        private void Start_1()
+        {
+            
+        }
+    }
+}
