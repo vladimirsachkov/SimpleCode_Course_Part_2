910a28d4292eff8839ed16641c7ff904e02f926d
vladimir <sachkovvi@shm.ru>
2025-02-01 12:18:08 +0300

    Пройден урок: ЗАПОЛНЕНИЕ ДВУМЕРНОГО МАССИВА С КЛАВИАТУРЫ C# | СЛУЧАЙНЫМИ ЧИСЛАМИ | C# | СИ ШАРП УРОКИ | УРОК # 32

--- Launcher.cs
+++ Launcher.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SimpleCode_Course_Part_2;
 
-Lesson_31 lesson_31 = new Lesson_31();
+Lesson_32 lesson_32 = new Lesson_32();
 
-lesson_31.Start();
+lesson_32.Start();
---
+++ Lessons/Lesson_32.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCode_Course_Part_2
+{
+    /*
+     * заполнение двумерного массива слуйчайными числами C#
+     * 
+     * заполнение двумерного массива с клавиатуры C#
+     */
+
+    class Lesson_32
+    {
+        public Lesson_32()
+        {
+        }
+
+        public void Start()
+        {
+            //Start_1();
+            //Start_2();
+            Start_3();
+        }
+
+        public void Start_1() 
+        {
+            int[,] myArray = new int[10,6];
+            Random random = new Random();
+
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    myArray[i, j] = random.Next();
+                }
+            }
+
+            for (int y = 0; y < myArray.GetLength(0); y++)
+            {
+                for (int x = 0; x < myArray.GetLength(1); x++)
+                {
+                    Console.Write(myArray[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Start_2()
+        {
+            int[,] myArray = new int[2, 3];
+
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.WriteLine("Y: " + i + " X: " + j);
+                    myArray[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int y = 0; y < myArray.GetLength(0); y++)
+            {
+                for (int x = 0; x < myArray.GetLength(1); x++)
+                {
+                    Console.Write(myArray[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Start_3()
+        {
+            string[,] myArray = new string[2, 3];
+
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.WriteLine("Y: " + i + " X: " + j);
+                    myArray[i, j] = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int y = 0; y < myArray.GetLength(0); y++)
+            {
+                for (int x = 0; x < myArray.GetLength(1); x++)
+                {
+                    Console.Write(myArray[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
