974c9dcf1ff1f0f652bbf40d587588549842c2e6
vladimir <sachkovvi@shm.ru>
2025-02-01 13:06:51 +0300

    Пройден урок: СТУПЕНЧАТЫЕ (ЗУБЧАТЫЕ) МАССИВЫ C# | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | УРОК # 33

--- Launcher.cs
+++ Launcher.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SimpleCode_Course_Part_2;
 
-Lesson_32 lesson_32 = new Lesson_32();
+Lesson_33 lesson_33 = new Lesson_33();
 
-lesson_32.Start();
+lesson_33.Start();
+
---
+++ Lessons/Lesson_33.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCode_Course_Part_2
+{
+    class Lesson_33
+    {
+        /*
+         * Ступенчатые (зубчатые массивы)
+         */
+
+        public Lesson_33()
+        {
+        }
+
+        public void Start()
+        {
+            //Start_1();
+            Start_2();
+        }
+
+        private void Start_1()
+        {
+            int[][] myArray = new int [3][];
+            //int[,] myArray2 = new int [3, 5];
+
+            //int myArrayRank = myArray.Rank;
+            //int myArray2Rank = myArray2.Rank;
+
+            //int myArrayLenght = myArray.Length;
+            //int myArray2Length = myArray2.Length;
+
+            myArray[0] = new int[5];
+            myArray[1] = new int[2];
+            myArray[2] = new int[10];
+
+            myArray[0][3] = 55;
+            int[] arr = myArray[0];
+
+            Console.WriteLine(myArray[0][3]);
+        }
+
+        private void Start_2() 
+        {
+            int[][] myArray = new int[3][];
+            myArray[0] = new int[5];
+            myArray[1] = new int[2];
+            myArray[2] = new int[10];
+
+            Random random = new Random();
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                for (int j = 0; j < myArray[i].Length; j++)
+                {
+                    myArray[i][j] = random.Next(100);
+                }
+            }
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                for (int j = 0; j < myArray[i].Length; j++)
+                {
+                    Console.Write(myArray[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
