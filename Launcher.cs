b16f1898636ae61bb41ced24012066361f56fe37
vladimir <sachkovvi@shm.ru>
2025-02-05 11:10:35 +0300

    Пройден урок: C# ФУНКЦИИ И МЕТОДЫ | МЕТОД C# ЧТО ЭТО | ФУНКЦИИ C# ПРИМЕР | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 35

--- Launcher.cs
+++ Launcher.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SimpleCode_Course_Part_2;
 
-Lesson_34 lesson_34 = new Lesson_34();
+//Lesson_34 lesson_34 = new Lesson_34();
 
-lesson_34.Start();
+//lesson_34.Start();
 
+Lesson_35.Start();
---
+++ Lessons/Lesson_35.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCode_Course_Part_2
+{
+    /*
+    *   Функции и методы в c#
+    */
+
+    //  модификаторы тип_возвращаемого_значения название_метода(параметры)
+    //  {
+    //      тело метода
+    //  }
+    
+    class Lesson_35
+    {
+
+        public static void Start()
+        {
+            //Start_1();
+            //Start_2();
+            Start_3();
+        }
+
+        private static void Start_1()
+        {
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            c = Sum(a, b);
+            
+            
+            Console.WriteLine(c);
+
+            Console.WriteLine("=====================");
+        }
+
+        private static void Start_2()
+        {
+            //PrintLine();
+            Random random = new Random();
+            Console.WriteLine(random.Next());
+        }
+
+        private static void Start_3()
+        {
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            c = Sum(a, b);
+
+
+            PrintResult(c);
+
+            Console.WriteLine("=====================");
+        }
+
+        static void PrintLine()
+        {
+            Console.WriteLine("Метод PrintLine был вызван!");
+        }
+
+        static void PrintResult(int result)
+        {
+            Console.WriteLine("Результат сложения: " + result);
+        }
+
+        static int Sum(int value_1, int value_2) 
+        {
+            return value_1 + value_2;
+        }
+
+        
+    }
+}
