2fd2b4fa7a5a32cd080b45937636778621b2c226
vladimir <sachkovvi@shm.ru>
2025-02-06 10:58:53 +0300

    Пройден урок: ОПЕРАТОР ОБЪЕДИНЕНИЯ С NULL С# | C# ?? | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 40

--- Launcher.cs
+++ Launcher.cs
@@ -5,4 +5,4 @@ using SimpleCode_Course_Part_2;
 
 //lesson_34.Start();
 
-Lesson_39.Start();
+Lesson_40.Start();
---
+++ Lessons/Lesson_40.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCode_Course_Part_2
+{
+    /*
+     *     оператор null-объединения   ??
+     */
+
+    /// <summary>
+    ///   ОПЕРАТОР ОБЪЕДИНЕНИЯ С NULL С# | C# ?? | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 40
+    /// </summary>
+    class Lesson_40
+    {
+        public Lesson_40()
+        {
+        }
+
+        public static void Start()
+        {
+            Start_3();
+        }
+
+        public static void Start_1()
+        {
+            string str = null;
+
+            string result;
+
+            if (str == null)
+            {
+                result = "нет данных";
+            } 
+            else
+            {
+                result = str;
+            }
+
+            Console.WriteLine(result);
+        }
+
+        public static void Start_2()
+        {
+            string str = null;
+
+            string result = str ?? "нет данных";
+
+            Console.WriteLine(result);
+        }
+
+        public static void Start_3()
+        {
+            string str = null;
+
+            string result = str ?? "";
+
+            Console.WriteLine("колличество символов в строке " + str.Length);
+        }
+    }
+}
