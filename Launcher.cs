285020969d25a932c2f9836f2e735aff701aa59c
vladimir <sachkovvi@shm.ru>
2025-03-23 13:02:54 +0300

    Пройден урок:  КЛЮЧЕВОЕ СЛОВО OUT C# | РАЗНИЦА между REF и OUT C# | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 44

--- Launcher.cs
+++ Launcher.cs
@@ -5,4 +5,4 @@ using SimpleCode_Course_Part_2;
 
 //lesson_34.Start();
 
-Lesson_43.Start();
+Lesson_44.Start();
---
+++ Lessons/Lesson_44.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCode_Course_Part_2
+{
+    /// <summary>
+    /// КЛЮЧЕВОЕ СЛОВО OUT C# | РАЗНИЦА между REF и OUT C# | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 44
+    /// 
+    /// Ключевое слово ref и out
+    /// </summary>
+    class Lesson_44
+    {
+        public Lesson_44()
+        {
+        }
+
+        public static void Start()
+        {
+            Start_3();
+        }
+
+        public static void Start_1() 
+        {
+            int a = 10;
+            Foo(ref a);
+        }
+
+        public static void Start_2() 
+        {
+            Bar(out int a);
+            Console.WriteLine(a);
+        }
+
+        public static void Start_3() 
+        {
+            string str = Console.ReadLine();
+            
+            int.TryParse(str, out int result);
+
+            Console.WriteLine(result);
+        }
+
+        public static void Foo(ref int value) 
+        { 
+            value++;
+            Console.WriteLine(value);
+        }
+
+        public static void Bar(out int value)
+        {
+            value = 10;
+        } 
+    }
+}
