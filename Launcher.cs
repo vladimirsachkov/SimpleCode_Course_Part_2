ec4985de0da620b8680d4a6955fcc2bbc7f92af6
vladimir <alexanderredar1@gmail.com>
2025-07-22 15:28:57 +0300

    Пройден урок: Что такое Nullable | Null совместимые значимые типы Nullable | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 52

--- Launcher.cs
+++ Launcher.cs
@@ -5,4 +5,4 @@ using SimpleCode_Course_Part_2;
 
 //lesson_34.Start();
 
-Lesson_51.Start();
+Lesson_52.Start();
---
+++ Lessons/Lesson_52.cs
@@ -0,0 +1,80 @@
+﻿namespace SimpleCode_Course_Part_2;
+
+/// <summary>
+/// Что такое Nullable | Null совместимые значимые типы Nullable |
+/// C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 52
+/// </summary>
+public class Lesson_52
+{
+    public Lesson_52()
+    {
+    }
+
+    public static void Start()
+    {
+        Start_1();
+        Start_2();
+        Start_3();
+        Start_4();
+        Start_5();
+        Start_6();
+    }
+
+    public static void Start_1()
+    {
+        string str = null;
+    
+        int? a = null;
+        // int? a = 2;
+        Console.WriteLine(a);
+        Console.WriteLine(a.GetValueOrDefault(5));
+    
+        // DateTime? dateTime = null;
+    }
+    
+    public static void Start_2()
+    {
+        int a = 5;
+        int? b = 22;
+        int? result = a + b;
+        Console.WriteLine(result);
+    }
+    
+    public static void Start_3()
+    {
+        int a = 5;
+        int? b = null;
+        int? result = a + b;
+        Console.WriteLine(result);
+    }
+    
+    public static void Start_4()
+    {
+        Console.WriteLine("Start_4");
+        int a = 5;
+        int? b = null;
+        Console.WriteLine("a == b: " + (a == b));
+        Console.WriteLine("a > b: " + (a > b));
+        Console.WriteLine("a < b: " + (a < b));
+    }
+    
+    public static void Start_5()
+    {
+        Console.WriteLine("Start_5");
+        int a = 5;
+        int? b = 5;
+        Console.WriteLine("a == b: " + (a == b));
+        Console.WriteLine("a > b: " + (a > b));
+        Console.WriteLine("a < b: " + (a < b));
+    }
+    
+    public static void Start_6()
+    {
+        Console.WriteLine("Start_6");
+        int a = 5;
+        int? b = 22;
+        Console.WriteLine("a == b: " + (a == b));
+        Console.WriteLine("a > b: " + (a > b));
+        Console.WriteLine("a < b: " + (a < b));
+    }
+}
