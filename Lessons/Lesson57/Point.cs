7a1ed79f98ad8d0b12b8b97ac1f64d06274efc94
vladimir <alexanderredar1@gmail.com>
2026-06-15 15:10:24 +0300

    Пройден урок: Модификаторы доступа C# | РАЗНИЦА МЕЖДУ public и private в C# | ООП C# | C# УРОК | # 57

---
+++ Lessons/Lesson57/Point.cs
@@ -0,0 +1,28 @@
+﻿namespace SimpleCode_Course_Part_2.Lesson57;
+
+/// <summary>
+/// Модификаторы доступа public и private, для
+/// членов класса
+/// </summary>
+public class Point
+{
+    int z = 3;
+    public int x = 1;
+    private int y = 44;
+
+    private void PrintX()
+    {
+        Console.WriteLine($"X: {x}");
+    }
+    
+    public void PrintY()
+    {
+        Console.WriteLine($"Y: {y}");
+    }
+
+    public void PrintPoint()
+    {
+        PrintX();
+        PrintY();
+    }
+}
---
+++ Lessons/Lesson_57.cs
@@ -0,0 +1,41 @@
+﻿using SimpleCode_Course_Part_2.Lesson57;
+using System.Reflection;
+
+namespace SimpleCode_Course_Part_2;
+
+/// <summary>
+/// Модификаторы доступа C#
+/// | РАЗНИЦА МЕЖДУ public и private в C#
+/// | ООП C#
+/// | C# УРОК
+/// | # 57
+/// </summary>
+public class Lesson_57
+{
+    public static void Start()
+    {
+        // Start_1();
+        Start_2();
+    }
+
+    static void Start_1()
+    {
+        Point point = new Point();
+
+        point.PrintPoint();
+    }
+    
+    static void Start_2()
+    {
+        var typeInfo = typeof(Point).GetFields(
+            BindingFlags.Instance |
+                     BindingFlags.NonPublic |
+                     BindingFlags.Public
+        );
+
+        foreach (var item in typeInfo)
+        {
+            Console.WriteLine($"{item.Name}\t IsPrivate: {item.IsPrivate}\t IsPublic: {item.IsPublic}");
+        }
+    }
+}
