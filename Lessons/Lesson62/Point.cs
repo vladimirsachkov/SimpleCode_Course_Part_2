0baf3028085a00339e1abdf38d729026bbd5bd0e
vladimir <alexanderredar1@gmail.com>
2026-06-16 14:19:05 +0300

    Пройден урок: C# свойства get set | ключевое слово value | автоматические свойства c# | ООП C# Урок | # 62

---
+++ Lessons/Lesson62/Point.cs
@@ -0,0 +1,54 @@
+﻿namespace SimpleCode_Course_Part_2.Lesson62;
+
+/// <summary>
+/// Свойства (Properties)
+///
+/// Автоматические свойства
+/// </summary>
+public class Point
+{
+    private int x;
+    private int y = 99;
+    public int Z { get; set; }
+
+    private int Y
+    {
+        // get => y;
+        set
+        {
+            if (value < 0)
+            {
+                y = 1;
+            }
+        
+            if (value > 5)
+            {
+                y = value;
+            }
+            y = value;
+        }
+    }
+
+
+    public void setX(int x)
+    {
+        if (x < 0)
+        {
+            this.x = 1;
+            return;
+        }
+        
+        if (x > 5)
+        {
+            this.x = 5;
+            return;
+        }
+        
+        this.x = x;
+    }
+    
+    public int getX()
+    {
+        return x;
+    }
+}
---
+++ Lessons/Lesson_62.cs
@@ -0,0 +1,31 @@
+﻿using SimpleCode_Course_Part_2.Lesson62;
+
+namespace SimpleCode_Course_Part_2;
+
+/// <summary>
+/// C# свойства get set
+/// | ключевое слово value
+/// | автоматические свойства c#
+/// | ООП C# Урок
+/// | # 62
+/// </summary>
+public class Lesson_62
+{
+    
+    public static void Start()
+    {
+        Start_1();
+    }
+
+    static void Start_1()
+    {
+        Point point = new Point();
+        point.setX(10);
+        
+        int x = point.getX();
+
+        // point.Y = 10;
+        
+        // int y = point.Y;
+    }
+}
