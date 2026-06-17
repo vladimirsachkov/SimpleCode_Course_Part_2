519e97b95f77396246145e138e0448312f7a9dba
vladimir <alexanderredar1@gmail.com>
2026-06-17 12:00:50 +0300

    Пройден урок: Статический класс c# | как работает ключевое слово static | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | # 66

---
+++ Lessons/Lesson_66.cs
@@ -0,0 +1,40 @@
+﻿namespace SimpleCode_Course_Part_2;
+
+/// <summary>
+/// Статический класс c#
+/// | как работает ключевое слово static
+/// | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ
+/// | # 66
+/// </summary>
+public class Lesson_66
+{
+    static class MyClass
+    {
+        static MyClass()
+        {
+            // this.
+        }
+        
+        public static void Foo()
+        {
+            Console.WriteLine("Foo");
+        }
+
+        public static void Bar()
+        {
+            Console.WriteLine("Bar");
+        }
+
+        static int a;
+    }
+    
+    public static void Start()
+    {
+        Start_1();
+    }
+    
+    static void Start_1()
+    {
+           
+    }
+}
